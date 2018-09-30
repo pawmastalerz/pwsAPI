f400432e9bef6577d814b60ec6fa698539619d56
Pawel <paw.mastalerz@gmail.com>
2018-09-30 16:32:16 +0200

    SQLite first test passed.

---
+++ Migrations/20180930142949_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pwsAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    PhotoUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.EventId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace pwsAPI.Models
+{
+    public class EventContext : DbContext
+    {
+        public EventContext(DbContextOptions<EventContext> options) : base(options) { }
+
+        public DbSet<Event> Events { get; set; }
+    }
+
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+
+}
