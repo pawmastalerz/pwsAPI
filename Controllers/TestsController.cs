143fc41b0043821603a2bb6081cd42be1ea4b16b
Pawel <paw.mastalerz@gmail.com>
2018-10-04 14:31:52 +0200

    Test Controller.

---
+++ Controllers/TestsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using pwsAPI.Data;
+
+namespace pwsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestsController : ControllerBase
+    {
+        private readonly IDataRepository repo;
+
+        public TestsController(IDataRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        // [Authorize]
+        public IActionResult Get()
+        {
+            return Ok("Test passed");
+        }
+
+    }
+}
